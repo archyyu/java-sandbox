38f99bac244d0aa41b68e114692b637a5fdd962b
archyyu <archyyu321@gmail.com>
2026-06-01 11:25:38 +0100

    add kafka_test python script test against the KafkaController

--- src/main/java/com/script/controller/KafkaController.java
+++ src/main/java/com/script/controller/KafkaController.java
@@ -1,12 +1,16 @@
 package com.script.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.script.service.KafkaService;
 
 import jakarta.annotation.Resource;
@@ -19,6 +23,8 @@ public class KafkaController {
     
     @Resource
     private KafkaService kafkaService;
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
 
     @PostMapping("/{topic}/{consumer}")
@@ -34,9 +40,15 @@ public class KafkaController {
     }
 
     @PostMapping("/{topic}/produce")
-    public ResponseEntity<Object> product(@PathVariable String topic, @RequestParam String body) {
+    public ResponseEntity<Object> product(@PathVariable String topic, @RequestBody String body) throws Exception {
         
-        this.kafkaService.produceMessage((Object)body, topic);
+        logger.info("topic:" + topic);
+        logger.info("body:" + body);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Object object = objectMapper.readValue(body, Object.class);
+
+        this.kafkaService.produceMessage(object, topic);
         return ResponseEntity.ok("");
 
     }
--- test/kafka_test.py
+++ test/kafka_test.py
@@ -2,4 +2,35 @@ import requests
 
 
 class Kafka:
+    def __init__(self, topic):
+        self.topic = topic
+        self.token = ""
+
+    def login(self, username, password):
+        data = {"username": username,"password": password}
+        response = requests.post("http://localhost:8080/api/auth/login", json=data).json()
+        self.token = response["token"]
+        print(self.token)
     
+    def produce(self, dict):
+        headers = {"Authorization": f"Bearer {self.token}"}
+        response = requests.post(f"http://localhost:8080/api/kafka/{self.topic}/produce", headers=headers, json=dict)
+        print(response)
+    
+    def consume(self, consumer):
+        headers = {"Authorization": f"Bearer {self.token}"}
+        response = requests.post(f"http://localhost:8080/api/kafka/{self.topic}/{consumer}", headers=headers).json()
+        print(response);
+
+
+kafka = Kafka("kafkapython")
+
+kafka.login("user1", "pass1")
+
+obj = {}
+obj["name"] = "name"
+obj["content"] = "content"
+
+kafka.produce(obj)
+
+kafka.consume("consumer")
